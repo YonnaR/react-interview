6725b586e164dc138bddd5ed20ffaf2846e80d2f
YYriina <yoann.forthulman@gmail.com>
2019-02-13 17:16:09 +0100

    Updates

--- precache-manifest.f5ecf201fb98ec5f36fafb506643f649.js
+++ precache-manifest.f5ecf201fb98ec5f36fafb506643f649.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-interview/static/js/runtime~main.37c739f1.js"
   },
   {
-    "revision": "bd2742947c00633083ce",
-    "url": "/react-interview/static/js/main.bd274294.chunk.js"
+    "revision": "a0325f8350ee5681cbed",
+    "url": "/react-interview/static/js/main.a0325f83.chunk.js"
   },
   {
     "revision": "aff1a99d9bc5ccb58f47",
     "url": "/react-interview/static/js/2.aff1a99d.chunk.js"
   },
   {
-    "revision": "bd2742947c00633083ce",
-    "url": "/react-interview/static/css/main.fdb4e0b7.chunk.css"
+    "revision": "a0325f8350ee5681cbed",
+    "url": "/react-interview/static/css/main.afe1ea34.chunk.css"
   },
   {
-    "revision": "7c49a27015210ce2d997c1da3c88e0ee",
+    "revision": "2e176b01efe38c2841a423628d999389",
     "url": "/react-interview/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-interview/precache-manifest.2d057d0a3d3b2d6f94e280ae06820093.js"
+  "/react-interview/precache-manifest.f5ecf201fb98ec5f36fafb506643f649.js"
 );
 
 workbox.clientsClaim();
